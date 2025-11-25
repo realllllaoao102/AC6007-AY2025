ef8bdf88c31268d8bf62bdada6001d5a64369337
realllllaoao102 <zhangliangao13@gmail.com>
2025-11-25 03:19:26 +0000

    1

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ def index():
 @app.route("/",methods=["GET","POST"])
 def main():
     return(render_template("main.html"))
-
+@app.route("/dbs",methods=["GET","POST"])
+def main():
+    return(render_template("dbs.html"))
 if __name__ == "__main__":
     app.run(port=1234)
     
